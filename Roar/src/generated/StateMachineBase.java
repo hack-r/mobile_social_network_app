1ec20db52e4905de2cf76d7a3c8af09d9b12da22
Jason Miller <root@hack-r.com>
2016-03-05 01:55:42 -0500

    init
    init

---
+++ Roar/src/generated/StateMachineBase.java
@@ -0,0 +1,466 @@
+/**
+ * This class contains generated code from the Codename One Designer, DO NOT MODIFY!
+ * This class is designed for subclassing that way the code generator can overwrite it
+ * anytime without erasing your changes which should exist in a subclass!
+ * For details about this file and how it works please read this blog post:
+ * http://codenameone.blogspot.com/2010/10/ui-builder-class-how-to-actually-use.html
+*/
+package generated;
+
+import com.codename1.ui.*;
+import com.codename1.ui.util.*;
+import com.codename1.ui.plaf.*;
+import java.util.Hashtable;
+import com.codename1.ui.events.*;
+
+public abstract class StateMachineBase extends UIBuilder {
+    private Container aboutToShowThisContainer;
+    /**
+     * this method should be used to initialize variables instead of
+     * the constructor/class scope to avoid race conditions
+     */
+    /**
+    * @deprecated use the version that accepts a resource as an argument instead
+    
+**/
+    protected void initVars() {}
+
+    protected void initVars(Resources res) {}
+
+    public StateMachineBase(Resources res, String resPath, boolean loadTheme) {
+        startApp(res, resPath, loadTheme);
+    }
+
+    public Container startApp(Resources res, String resPath, boolean loadTheme) {
+        initVars();
+        UIBuilder.registerCustomComponent("Form", com.codename1.ui.Form.class);
+        UIBuilder.registerCustomComponent("Button", com.codename1.ui.Button.class);
+        UIBuilder.registerCustomComponent("Label", com.codename1.ui.Label.class);
+        UIBuilder.registerCustomComponent("TextField", com.codename1.ui.TextField.class);
+        if(loadTheme) {
+            if(res == null) {
+                try {
+                    if(resPath.endsWith(".res")) {
+                        res = Resources.open(resPath);
+                        System.out.println("Warning: you should construct the state machine without the .res extension to allow theme overlays");
+                    } else {
+                        res = Resources.openLayered(resPath);
+                    }
+                } catch(java.io.IOException err) { err.printStackTrace(); }
+            }
+            initTheme(res);
+        }
+        if(res != null) {
+            setResourceFilePath(resPath);
+            setResourceFile(res);
+            initVars(res);
+            return showForm(getFirstFormName(), null);
+        } else {
+            Form f = (Form)createContainer(resPath, getFirstFormName());
+            initVars(fetchResourceFile());
+            beforeShow(f);
+            f.show();
+            postShow(f);
+            return f;
+        }
+    }
+
+    protected String getFirstFormName() {
+        return "Main";
+    }
+
+    public Container createWidget(Resources res, String resPath, boolean loadTheme) {
+        initVars();
+        UIBuilder.registerCustomComponent("Form", com.codename1.ui.Form.class);
+        UIBuilder.registerCustomComponent("Button", com.codename1.ui.Button.class);
+        UIBuilder.registerCustomComponent("Label", com.codename1.ui.Label.class);
+        UIBuilder.registerCustomComponent("TextField", com.codename1.ui.TextField.class);
+        if(loadTheme) {
+            if(res == null) {
+                try {
+                    res = Resources.openLayered(resPath);
+                } catch(java.io.IOException err) { err.printStackTrace(); }
+            }
+            initTheme(res);
+        }
+        return createContainer(resPath, "Main");
+    }
+
+    protected void initTheme(Resources res) {
+            String[] themes = res.getThemeResourceNames();
+            if(themes != null && themes.length > 0) {
+                UIManager.getInstance().setThemeProps(res.getTheme(themes[0]));
+            }
+    }
+
+    public StateMachineBase() {
+    }
+
+    public StateMachineBase(String resPath) {
+        this(null, resPath, true);
+    }
+
+    public StateMachineBase(Resources res) {
+        this(res, null, true);
+    }
+
+    public StateMachineBase(String resPath, boolean loadTheme) {
+        this(null, resPath, loadTheme);
+    }
+
+    public StateMachineBase(Resources res, boolean loadTheme) {
+        this(res, null, loadTheme);
+    }
+
+    public com.codename1.ui.Button findSubmitRoar(Component root) {
+        return (com.codename1.ui.Button)findByName("submitRoar", root);
+    }
+
+    public com.codename1.ui.Button findSubmitRoar() {
+        com.codename1.ui.Button cmp = (com.codename1.ui.Button)findByName("submitRoar", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Button)findByName("submitRoar", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Button findWallButton(Component root) {
+        return (com.codename1.ui.Button)findByName("wallButton", root);
+    }
+
+    public com.codename1.ui.Button findWallButton() {
+        com.codename1.ui.Button cmp = (com.codename1.ui.Button)findByName("wallButton", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Button)findByName("wallButton", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Button findRefreshButton(Component root) {
+        return (com.codename1.ui.Button)findByName("refreshButton", root);
+    }
+
+    public com.codename1.ui.Button findRefreshButton() {
+        com.codename1.ui.Button cmp = (com.codename1.ui.Button)findByName("refreshButton", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Button)findByName("refreshButton", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Label findLabel(Component root) {
+        return (com.codename1.ui.Label)findByName("Label", root);
+    }
+
+    public com.codename1.ui.Label findLabel() {
+        com.codename1.ui.Label cmp = (com.codename1.ui.Label)findByName("Label", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Label)findByName("Label", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.TextField findTextField(Component root) {
+        return (com.codename1.ui.TextField)findByName("TextField", root);
+    }
+
+    public com.codename1.ui.TextField findTextField() {
+        com.codename1.ui.TextField cmp = (com.codename1.ui.TextField)findByName("TextField", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.TextField)findByName("TextField", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public com.codename1.ui.Button findNavRoarButton(Component root) {
+        return (com.codename1.ui.Button)findByName("navRoarButton", root);
+    }
+
+    public com.codename1.ui.Button findNavRoarButton() {
+        com.codename1.ui.Button cmp = (com.codename1.ui.Button)findByName("navRoarButton", Display.getInstance().getCurrent());
+        if(cmp == null && aboutToShowThisContainer != null) {
+            cmp = (com.codename1.ui.Button)findByName("navRoarButton", aboutToShowThisContainer);
+        }
+        return cmp;
+    }
+
+    public static final int COMMAND_WallIWantToRoar = 2;
+    public static final int COMMAND_MainGoToYourWall = 1;
+
+    protected boolean onWallIWantToRoar() {
+        return false;
+    }
+
+    protected boolean onMainGoToYourWall() {
+        return false;
+    }
+
+    protected void processCommand(ActionEvent ev, Command cmd) {
+        switch(cmd.getId()) {
+            case COMMAND_WallIWantToRoar:
+                if(onWallIWantToRoar()) {
+                    ev.consume();
+                    return;
+                }
+                break;
+
+            case COMMAND_MainGoToYourWall:
+                if(onMainGoToYourWall()) {
+                    ev.consume();
+                    return;
+                }
+                break;
+
+        }
+        if(ev.getComponent() != null) {
+            handleComponentAction(ev.getComponent(), ev);
+        }
+    }
+
+    protected void exitForm(Form f) {
+        if("Wall".equals(f.getName())) {
+            exitWall(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(f.getName())) {
+            exitMain(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+            return;
+    }
+
+
+    protected void exitWall(Form f) {
+    }
+
+
+    protected void exitMain(Form f) {
+    }
+
+    protected void beforeShow(Form f) {
+    aboutToShowThisContainer = f;
+        if("Wall".equals(f.getName())) {
+            beforeWall(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(f.getName())) {
+            beforeMain(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+            return;
+    }
+
+
+    protected void beforeWall(Form f) {
+    }
+
+
+    protected void beforeMain(Form f) {
+    }
+
+    protected void beforeShowContainer(Container c) {
+        aboutToShowThisContainer = c;
+        if("Wall".equals(c.getName())) {
+            beforeContainerWall(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(c.getName())) {
+            beforeContainerMain(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+            return;
+    }
+
+
+    protected void beforeContainerWall(Container c) {
+    }
+
+
+    protected void beforeContainerMain(Container c) {
+    }
+
+    protected void postShow(Form f) {
+        if("Wall".equals(f.getName())) {
+            postWall(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(f.getName())) {
+            postMain(f);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+            return;
+    }
+
+
+    protected void postWall(Form f) {
+    }
+
+
+    protected void postMain(Form f) {
+    }
+
+    protected void postShowContainer(Container c) {
+        if("Wall".equals(c.getName())) {
+            postContainerWall(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(c.getName())) {
+            postContainerMain(c);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+            return;
+    }
+
+
+    protected void postContainerWall(Container c) {
+    }
+
+
+    protected void postContainerMain(Container c) {
+    }
+
+    protected void onCreateRoot(String rootName) {
+        if("Wall".equals(rootName)) {
+            onCreateWall();
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(rootName)) {
+            onCreateMain();
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+            return;
+    }
+
+
+    protected void onCreateWall() {
+    }
+
+
+    protected void onCreateMain() {
+    }
+
+    protected Hashtable getFormState(Form f) {
+        Hashtable h = super.getFormState(f);
+        if("Wall".equals(f.getName())) {
+            getStateWall(f, h);
+            aboutToShowThisContainer = null;
+            return h;
+        }
+
+        if("Main".equals(f.getName())) {
+            getStateMain(f, h);
+            aboutToShowThisContainer = null;
+            return h;
+        }
+
+            return h;
+    }
+
+
+    protected void getStateWall(Form f, Hashtable h) {
+    }
+
+
+    protected void getStateMain(Form f, Hashtable h) {
+    }
+
+    protected void setFormState(Form f, Hashtable state) {
+        super.setFormState(f, state);
+        if("Wall".equals(f.getName())) {
+            setStateWall(f, state);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+        if("Main".equals(f.getName())) {
+            setStateMain(f, state);
+            aboutToShowThisContainer = null;
+            return;
+        }
+
+            return;
+    }
+
+
+    protected void setStateWall(Form f, Hashtable state) {
+    }
+
+
+    protected void setStateMain(Form f, Hashtable state) {
+    }
+
+    protected void handleComponentAction(Component c, ActionEvent event) {
+        Container rootContainerAncestor = getRootAncestor(c);
+        if(rootContainerAncestor == null) return;
+        String rootContainerName = rootContainerAncestor.getName();
+        Container leadParentContainer = c.getParent().getLeadParent();
+        if(leadParentContainer != null && leadParentContainer.getClass() != Container.class) {
+            c = c.getParent().getLeadParent();
+        }
+        if(rootContainerName == null) return;
+        if(rootContainerName.equals("Wall")) {
+            if("navRoarButton".equals(c.getName())) {
+                onWall_NavRoarButtonAction(c, event);
+                return;
+            }
+            if("refreshButton".equals(c.getName())) {
+                onWall_RefreshButtonAction(c, event);
+                return;
+            }
+        }
+        if(rootContainerName.equals("Main")) {
+            if("TextField".equals(c.getName())) {
+                onMain_TextFieldAction(c, event);
+                return;
+            }
+            if("submitRoar".equals(c.getName())) {
+                onMain_SubmitRoarAction(c, event);
+                return;
+            }
+            if("wallButton".equals(c.getName())) {
+                onMain_WallButtonAction(c, event);
+                return;
+            }
+        }
+    }
+
+      protected void onWall_NavRoarButtonAction(Component c, ActionEvent event) {
+      }
+
+      protected void onWall_RefreshButtonAction(Component c, ActionEvent event) {
+      }
+
+      protected void onMain_TextFieldAction(Component c, ActionEvent event) {
+      }
+
+      protected void onMain_SubmitRoarAction(Component c, ActionEvent event) {
+      }
+
+      protected void onMain_WallButtonAction(Component c, ActionEvent event) {
+      }
+
+}
